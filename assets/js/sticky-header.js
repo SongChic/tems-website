6b9f7684473617bfd9aa0a4fda63335a9ad72464
SongChic <song_chic@naver.com>
2016-03-01 02:14:19 +0900

    sticky header add and red.css modify

---
+++ assets/js/sticky-header.js
@@ -0,0 +1,24 @@
+$.fn.sticktHeader = function (option) {
+    var $target = $(this),
+        targetOffset = $target.offset().top,
+        $empty = $target.prev(),
+        $parent = $(this).closest(".wrapper"),
+        state = false;
+
+    $(window).on("scroll", function(event) {
+        if ($(this).scrollTop() > targetOffset) {
+            if ($parent.find(".empty").length == 0) {
+                $target.before("<div class='empty'>")
+            }
+            $target.addClass("sticky");
+        } else {
+            $target.removeClass("sticky");
+            $parent.find(".empty").remove();
+        }
+        console.log("a");
+    });
+
+    function addEmpty() {
+
+    }
+};
