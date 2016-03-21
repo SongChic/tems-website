9b0cfad5c1c476383685898b6a891223dbe37a05
Robin Rheem <haaaaaaappy.911@gmail.com>
2016-03-21 22:54:56 +0900

    Fixed Jays requests.

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,15 +3,15 @@ jQuery(document).ready(function(){
 
     map = new GMaps({
         div: '#map',
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 43.467866,
+        lng:  -80.520608,
     });
     map.addMarker({
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 43.467866,
+        lng:  -80.520608,
         title: 'Address',      
         infoWindow: {
-            content: '<h5 class="title">College Green Campus</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
+            content: '<h5 class="title">TEMS Academy</h5><p><span class="region">Canada TEMS Academy 33 Bridgeport Road East,</span><br><span class="postal-code">Waterloo, Ontario N2J 2J4</span><br><span class="country-name">Canada</span></p>'
         }
         
     });
